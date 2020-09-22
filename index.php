1f6f903c11e83e5875097d92b01ccfdfd2d60fb7
Dwiki <dwiki.likuisa@spesolution.com>
2020-09-22 17:40:16 +0700

    try fix 404

--- index.php
+++ index.php
@@ -2,7 +2,39 @@
 <html lang="en">
 
 <head>
-  <?php include_once 'layout/head.php'; ?>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>WaterioT</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/produk/logo-1.jpeg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <?php 
+    include_once 'config/dao.php';
+    $dao = new Dao();
+    $result = $dao->selectAll('profil');
+    $profil = array();
+    foreach ($result as $key => $value) {
+        $profil[$value['label']] = $value['value'];
+    }
+    ?>
 </head>
 
 <body>
@@ -13,7 +45,16 @@
 
       <div class="row justify-content-center">
         <div class="col-xl-9 d-flex align-items-center">
-          <?php include_once 'layout/nav_menu.php'; ?>
+        <a href="index" class="logo mr-auto"><img src="assets/img/produk/logo-2.png" alt="" class="img-fluid"></a>
+        <nav class="nav-menu d-none d-lg-block">
+          <ul>
+            <li class="active"><a href="index#hero">Home</a></li>
+            <li><a href="index#about">About</a></li>
+            <li><a href="index#services">Services</a></li>
+            <li><a href="index#portfolio">Portfolio</a></li>
+          </ul>
+        </nav><!-- .nav-menu -->
+        <a href="index#contact" class="get-started-btn scrollto">Contact Us</a>
         </div>
       </div>
 
@@ -301,14 +342,99 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <?php include_once 'layout/footer.php'; ?>
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 footer-contact">
+            <h3>WaterioT</h3>
+            <p>
+              <?= $profil['alamat'] ?><br>
+              <?= $profil['kabupaten'] ?><br>
+              <?= $profil['provinsi'] ?><br><br>
+              <strong>Phone:</strong> <?= $profil['phone_number'] ?><br>
+              <strong>Email:</strong> <?= $profil['email'] ?><br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Menu</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index#hero">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index#about">About</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index#services">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index#portfolio">Portfolio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index#contact">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+            <?php
+              $result = $dao->selectAll('services');
+              foreach ($result as $value) {
+                ?>
+                <li><i class="bx bx-chevron-right"></i> <a href="#"><?= $value['nama'] ?></a></li>
+                <?php
+              }
+              ?>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Product Selling</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Consultant Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Rental Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Training Services</a></li> -->
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="copyright-wrap d-md-flex py-4">
+        <div class="mr-md-auto text-center text-md-left">
+          <div class="copyright">
+            &copy; Copyright <strong><span>WaterioT</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/techie-free-skin-bootstrap-3/ -->
+          </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </footer>
   <!-- End Footer -->
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <?php include_once 'layout/js.php'; ?>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 
 </body>
 
